1238f7c3f5c5a537e287f6e6c4789c0ea8649d27
liangtao <547670718@qq.com>
2024-04-12 09:50:28 +0800

    完善example

--- example/src/main/java/example/TikTokensExample.java
+++ example/src/main/java/example/TikTokensExample.java
@@ -1,7 +1,6 @@
 package example;
 
-import com.theokanning.openai.completion.chat.ChatMessage;
-import com.theokanning.openai.completion.chat.SystemMessage;
+import com.theokanning.openai.completion.chat.*;
 import com.theokanning.openai.utils.TikTokensUtil;
 
 import java.util.ArrayList;
@@ -13,8 +12,10 @@ class TikTokensExample {
         List<ChatMessage> messages = new ArrayList<>();
         messages.add(new SystemMessage("Hello OpenAI 1."));
         messages.add(new SystemMessage("Hello OpenAI 2.   "));
+        messages.add(new UserMessage(new ImageContent("text", "textContent", new ImageUrl("dddd"))));
         int tokens_1 = TikTokensUtil.tokens(TikTokensUtil.ModelEnum.GPT_3_5_TURBO.getName(), messages);
         int tokens_2 = TikTokensUtil.tokens(TikTokensUtil.ModelEnum.GPT_3_5_TURBO.getName(), "Hello OpenAI 1.");
+        int tokens_3 = TikTokensUtil.tokens(TikTokensUtil.ModelEnum.GPT_4_TURBO.getName(), messages);
     }
 
 }
