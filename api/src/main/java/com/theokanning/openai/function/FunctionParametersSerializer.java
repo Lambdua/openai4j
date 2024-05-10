5d23368d9431c7c3d08423190fe684c9672437a2
liangtao <547670718@qq.com>
2024-05-10 15:01:12 +0800

    bug: fix stream_option 序列化问题

--- api/src/main/java/com/theokanning/openai/function/FunctionParametersSerializer.java
+++ api/src/main/java/com/theokanning/openai/function/FunctionParametersSerializer.java
@@ -9,15 +9,25 @@ import com.kjetland.jackson.jsonSchema.JsonSchemaGenerator;
 
 import java.io.IOException;
 
-public class FunctionParametersSerializer extends JsonSerializer<Class<?>> {
+public class FunctionParametersSerializer extends JsonSerializer<FunctionDefinition> {
 
     private final ObjectMapper mapper = new ObjectMapper();
     private final JsonSchemaConfig config = JsonSchemaConfig.vanillaJsonSchemaDraft4();
     private final JsonSchemaGenerator jsonSchemaGenerator = new JsonSchemaGenerator(mapper, config);
 
     @Override
-    public void serialize(Class<?> value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-
+    public void serialize(FunctionDefinition value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeStartObject();
+        gen.writeStringField("name", value.getName());
+        gen.writeStringField("description", value.getDescription());
+        if (value.getParametersDefinitionClass() != null) {
+            gen.writeFieldName("parameters");
+            gen.writeRawValue(mapper.writeValueAsString(jsonSchemaGenerator.generateJsonSchema(value.getParametersDefinitionClass())));
+        } else {
+            gen.writeFieldName("parameters");
+            gen.writeRawValue(mapper.writeValueAsString(value.getParametersDefinition()));
+        }
+        gen.writeEndObject();
     }
 
 
