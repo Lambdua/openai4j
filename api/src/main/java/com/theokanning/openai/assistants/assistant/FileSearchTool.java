4c45dda69388af3bcede85229a0625295708ac25
liangtao <547670718@qq.com>
2024-06-25 17:23:33 +0800

    构造函数补全

--- api/src/main/java/com/theokanning/openai/assistants/assistant/FileSearchTool.java
+++ api/src/main/java/com/theokanning/openai/assistants/assistant/FileSearchTool.java
@@ -6,9 +6,13 @@ package com.theokanning.openai.assistants.assistant;
  **/
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FileSearchTool implements Tool {
     final String type = "file_search";
 
