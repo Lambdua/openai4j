dc09dcb57be38ab25a8c459e21b83ee7d8b542aa
liangtao <547670718@qq.com>
2024-06-25 17:22:13 +0800

    max_num_results parameters add

--- api/src/main/java/com/theokanning/openai/assistants/assistant/FileSearchTool.java
+++ api/src/main/java/com/theokanning/openai/assistants/assistant/FileSearchTool.java
@@ -5,9 +5,16 @@ package com.theokanning.openai.assistants.assistant;
  * @date 2024年04月18 13:36
  **/
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class FileSearchTool implements Tool {
     final String type = "file_search";
+
+    /**
+     * The maximum number of results the file search tool should output. The default is 20 for gpt-4* models and 5 for gpt-3.5-turbo. This number should be between 1 and 50 inclusive.
+     */
+    @JsonProperty("max_num_results")
+    Integer maxNumResults;
 }
